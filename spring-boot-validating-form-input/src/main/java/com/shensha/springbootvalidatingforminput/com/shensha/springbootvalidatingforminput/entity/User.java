6bd868e897d5103aef0f48587eebd837ca67eb23
shensha <shensha@gevent.com.cn>
2018-08-15 16:28:57 +0800

    validate user

--- spring-boot-validating-form-input/src/main/java/com/shensha/springbootvalidatingforminput/com/shensha/springbootvalidatingforminput/entity/User.java
+++ spring-boot-validating-form-input/src/main/java/com/shensha/springbootvalidatingforminput/com/shensha/springbootvalidatingforminput/entity/User.java
@@ -56,4 +56,13 @@ public class User implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", birthday=" + birthDay +
+                '}';
+    }
 }
--- spring-boot-validating-form-input/src/main/java/com/shensha/springbootvalidatingforminput/com/shensha/springbootvalidatingforminput/service/impl/UserServiceImpl.java
+++ spring-boot-validating-form-input/src/main/java/com/shensha/springbootvalidatingforminput/com/shensha/springbootvalidatingforminput/service/impl/UserServiceImpl.java
@@ -1,7 +1,11 @@
 package com.shensha.springbootvalidatingforminput.com.shensha.springbootvalidatingforminput.service.impl;
 
+import com.shensha.springbootvalidatingforminput.com.shensha.springbootvalidatingforminput.dao.UserDao;
 import com.shensha.springbootvalidatingforminput.com.shensha.springbootvalidatingforminput.entity.User;
 import com.shensha.springbootvalidatingforminput.com.shensha.springbootvalidatingforminput.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,28 +13,41 @@ import java.util.List;
 @Service("userService")
 public class UserServiceImpl implements UserService {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    @Autowired
+    UserDao userDao;
+
+
     @Override
     public List<User> findAll() {
-        return null;
+        return userDao.findAll();
     }
 
     @Override
     public User insertByUser(User user) {
-        return null;
+        LOGGER.info("新增用户:"+user.toString());
+        return userDao.save(user);
     }
 
     @Override
     public User update(User user) {
-        return null;
+        LOGGER.info("更新用户:"+user.toString());
+        return userDao.save(user);
     }
 
     @Override
     public User delete(Long id) {
-        return null;
+        User user = userDao.findById(id).get();
+        userDao.delete(user);
+        LOGGER.info("删除用户"+user.toString());
+        return user;
     }
 
     @Override
     public User findById(Long id) {
-        return null;
+        User user = userDao.findById(id).get();
+        LOGGER.info("获取用户"+user.toString());
+        return user;
     }
 }
