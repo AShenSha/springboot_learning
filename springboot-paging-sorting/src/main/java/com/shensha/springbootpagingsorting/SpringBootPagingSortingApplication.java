22add680c3fcc1c71167f2dfa8db3eab5eecdd92
shensha <shensha@gevent.com.cn>
2018-08-23 23:19:54 +0800

    merge

---
+++ springboot-paging-sorting/src/main/java/com/shensha/springbootpagingsorting/SpringBootPagingSortingApplication.java
@@ -0,0 +1,12 @@
+package com.shensha.springbootpagingsorting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPagingSortingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPagingSortingApplication.class, args);
+    }
+}
---
+++ springboot-paging-sorting/src/main/java/com/shensha/springbootpagingsorting/entity/Book.java
@@ -0,0 +1,56 @@
+package com.shensha.springbootpagingsorting.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+@Entity
+public class Book implements Serializable {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotEmpty(message = "姓名不能为空")
+    private String name;
+
+    @NotEmpty(message = "作者不能为空")
+    private String writer;
+
+    @NotEmpty(message = "简介不能为空")
+    private String introduction;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getIntroduction() {
+        return introduction;
+    }
+
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+}
