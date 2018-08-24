fad3aba2c1927fa22cc51ba8f6a0de0b5ac35640
shensha <shensha@gevent.com.cn>
2018-08-24 17:19:28 +0800

    freemarker

--- springboot-freemarker/src/main/java/com/shensha/demo/DemoApplication.java
+++ springboot-freemarker/src/main/java/com/shensha/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.shensha.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.shensha.demo.dao")
 public class DemoApplication {
 
     public static void main(String[] args) {
