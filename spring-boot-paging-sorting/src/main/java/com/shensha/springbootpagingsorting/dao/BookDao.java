8b4d2ffc7223b64dd21e76f2f33325a1d5d143dc
shensha <shensha@gevent.com.cn>
2018-08-24 16:00:01 +0800

    Change project structure

--- spring-boot-paging-sorting/src/main/java/com/shensha/springbootpagingsorting/dao/BookDao.java
+++
@@ -1,8 +0,0 @@
-package com.shensha.springbootpagingsorting.dao;
-
-import com.shensha.springbootpagingsorting.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface BookDao extends JpaRepository<Book,Long>, JpaSpecificationExecutor<Book> {
-}
